d9577e6c709b54b7303f4e57951a158e073a3057
JoseVitorNobre <62249331+JoseVitorNobre@users.noreply.github.com>
2024-11-29 19:50:28 +0000

    finalizei atividade 01

--- atividade_01/src/main.rs
+++ atividade_01/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    use restaurant::front_of_house::hosting;
+    use restaurant::back_of_house;
+    use restaurant::PI;
+
+    hosting::add_to_wait_list();
+    hosting::seat_at_table();
+
+    back_of_house::take_care_trash();
+
+    println!("The value of PI is: {}", PI);
 }
